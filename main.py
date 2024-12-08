f52190d16cb26e84380f0d322643d8559daaa338
abdelwahab-7 <s-abdelwahab.abdelkawy@zewailcity.edu.eg>
2024-12-08 20:28:18 +0200

    initial recognition using SVM(ML model)

--- main.py
+++ main.py
@@ -1,58 +1,63 @@
 import cv2 as cv
 import numpy as np
 import os
-
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-import pickle
-import tensorflow as tf
+from mtcnn import MTCNN  # Import MTCNN
 from sklearn.preprocessing import LabelEncoder
 from keras_facenet import FaceNet
+import pickle
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-# Initialization
 facenet = FaceNet()
 faces_embeddings = np.load("faces_embeddings_done_4classes.npz")
 
 Y = faces_embeddings["arr_1"]
 encoder = LabelEncoder()
 encoder.fit(Y)
-haarcascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 model = pickle.load(open("svm_model_160x160.pkl", "rb"))
 
+detector = MTCNN()
+
 cap = cv.VideoCapture(0)
 
-
 while cap.isOpened():
-    _, frame = cap.read()
+    ret, frame = cap.read()
+    if not ret:
+        break
+
     rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    faces = haarcascade.detectMultiScale(gray_img, 1.3, 5)
-    for x, y, w, h in faces:
-        img = rgb_img[y : y + h, x : x + w]
-        img = cv.resize(img, (160, 160))  # 160x160x3
-        img = np.expand_dims(img, axis=0)  # Nonex160x160x3
+    detections = detector.detect_faces(rgb_img)
 
-        ypred = facenet.embeddings(img)
-        face_name = model.predict(ypred)
-        final_name = encoder.inverse_transform(face_name)[0]
+    for detection in detections:
+        x, y, w, h = detection["box"]
+        confidence = detection["confidence"]
 
-        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 10)
-        cv.putText(
-            frame,
-            str(final_name),
-            (x, y - 10),
-            cv.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 0, 255),
-            3,
-            cv.LINE_AA,
-        )
+        if confidence > 0.90:
+            face = rgb_img[y : y + h, x : x + w]
+            face = cv.resize(face, (160, 160))
+            face = np.expand_dims(face, axis=0)
 
-    cv.imshow("Face Recognition:", frame)
-    if cv.waitKey(1) & ord("q") == 27:
+            ypred = facenet.embeddings(face)
+            face_name = model.predict(ypred)
+            final_name = encoder.inverse_transform(face_name)[0]
+
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv.putText(
+                frame,
+                final_name,
+                (x, y - 10),
+                cv.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 0, 255),
+                2,
+                cv.LINE_AA,
+            )
+
+    cv.imshow("Face Recognition", frame)
+    if cv.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
-cv.destroyAllWindows
+cv.destroyAllWindows()
